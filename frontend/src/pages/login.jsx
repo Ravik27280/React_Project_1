9cd51527adeca14ab303b661c57e05796f60ae21
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2024-12-12 01:40:35 +0530

    add frontend setup

---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline bg-green-600">Hello world!</h1>
+      
+    </div>
+  )
+}
+
+export default Login
